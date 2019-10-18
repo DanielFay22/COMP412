98b9886a7ef2dd0afe11b4f892e4c7b16e064cca
DanielFay22 <dpf2@rice.edu>
2019-10-18 15:09:29 -0500

    Adds some stuff for allocating registers

--- allocator/allocate_registers.py
+++ allocator/allocate_registers.py
@@ -1,5 +1,6 @@
 
 from core import InternalRepresentation
+from resources import *
 
 
 
@@ -18,5 +19,46 @@ def allocate_registers(ir: InternalRepresentation, k: int):
     """
 
     """
+
+    pr_to_vr = [None] * k
+    vr_to_pr = [None] * ir.max_reg
+    vr_spill = vr_to_pr[:]
+
+    for op in ir.ir:
+        vr1 = op[IR_VR1]
+        vr2 = op[IR_VR2]
+        vr3 = op[IR_VR3]
+
+        if vr1 is not None:
+            if vr_to_pr[vr1] is not None:
+                op[IR_PR1] = vr_to_pr[vr1]
+            else:
+                # Retrieve spilled value
+                pass
+
+        if vr2 is not None:
+            if vr_to_pr[vr2] is not None:
+                op[IR_PR2] = vr_to_pr[vr2]
+            else:
+                # Retrieve spilled value
+                pass
+
+        if vr3 is not None:
+            try:
+                i = pr_to_vr.index(None)
+
+                vr_to_pr[vr3] = i
+                pr_to_vr[i] = vr3
+                op[IR_PR3] = i
+
+            except ValueError:
+                # Spill value
+                pass
+
+        
+
+
+
+
 
     pass
